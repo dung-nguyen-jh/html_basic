e9e2b248a951d0868d44299f2aff674082abb285
dung-nguyen-jh <dung.nguyen.2@journeyh.io>
2024-03-19 17:22:41 +0700

    Update map box convention

--- mapbox/scripts.js
+++ mapbox/scripts.js
@@ -4,7 +4,7 @@ function init() {
     limit: 5,
     zoom: 9,
     language: "en",
-  }
+  };
   const ACCESS_TOKEN =
     "pk.eyJ1IjoidGhpbmhsZTk5IiwiYSI6ImNsdHd1M3RjejAxYmwyaXBvbWRvN3VldW0ifQ.u_n9TO0Rr00-a65HjZ-0Fw";
 
@@ -16,13 +16,13 @@ function init() {
     zoom: config.zoom,
   });
 
-  const getAuthInfo = ()=>{
+  const getAuthInfo = () => {
     const sessionToken = Math.random().toString(36).substring(2, 15);
-    return{
+    return {
       access_token: mapboxgl.accessToken,
-      session_token: sessionToken
-    }
-  }
+      session_token: sessionToken,
+    };
+  };
 
   const searchJS = document.getElementById("search-js");
   searchJS.onload = function () {
@@ -65,7 +65,10 @@ function init() {
       .setHTML("<h3>Marker</h3><p>" + msg + "</p>")
       .addTo(map);
 
-    let marker = new mapboxgl.Marker().setLngLat(lngLat).setPopup(popup).addTo(map);
+    let marker = new mapboxgl.Marker()
+      .setLngLat(lngLat)
+      .setPopup(popup)
+      .addTo(map);
 
     marker.getElement().addEventListener("click", function (event) {
       console.log("You clicked on the marker");
@@ -75,75 +78,73 @@ function init() {
     });
   }
 
-  async function getSuggestion (searchInput){
-    try{
-    const url = "https://api.mapbox.com/search/searchbox/v1/suggest";
-    const auth = getAuthInfo();
-    const params = {
-      ...auth,
-      q: searchInput,
-      language: config.language,
-      limit: config.limit
-    };
-    const suggestionsResponse = await fetch(`${url}?${ new URLSearchParams(params).toString()}`);
-    const suggestionsData = await suggestionsResponse.json();
-    console.log(suggestionsData);
-    return suggestionsData;
-    }
-    catch(error){
+  async function getSuggestion(searchInput) {
+    try {
+      const url = "https://api.mapbox.com/search/searchbox/v1/suggest";
+      const auth = getAuthInfo();
+      const params = {
+        ...auth,
+        q: searchInput,
+        language: config.language,
+        limit: config.limit,
+      };
+      const suggestionsResponse = await fetch(
+        `${url}?${new URLSearchParams(params).toString()}`
+      );
+      const suggestionsData = await suggestionsResponse.json();
+      console.log(suggestionsData);
+      return suggestionsData;
+    } catch (error) {
       console.error("Suggestion fetch error:", error);
     }
   }
 
-  async function getLocationData(locationId){
+  async function getLocationData(locationId) {
     const retrieveParams = getAuthInfo();
-      try{
-          const retrieveUrl = `https://api.mapbox.com/search/searchbox/v1/retrieve/${encodeURIComponent(
-            locationId)}?${new URLSearchParams(retrieveParams).toString()}`;
-          // console.log(retrieveUrl);
-            const resultResponse = await fetch(retrieveUrl);
-            const resultData = await resultResponse.json();
-            // console.log(data);
-            return resultData;
-            
-          }
-          catch(error){
-            console.error("Fetch location details error:", error);
-          }
+    try {
+      const retrieveUrl = `https://api.mapbox.com/search/searchbox/v1/retrieve/${encodeURIComponent(
+        locationId
+      )}?${new URLSearchParams(retrieveParams).toString()}`;
+      // console.log(retrieveUrl);
+      const resultResponse = await fetch(retrieveUrl);
+      const resultData = await resultResponse.json();
+      // console.log(data);
+      return resultData;
+    } catch (error) {
+      console.error("Fetch location details error:", error);
+    }
   }
 
-  async function onSearchResultClick(event){
-    try{
-            const locationData = await getLocationData(event.target.id);
-            map.flyTo({
-              center: locationData.features[0].geometry.coordinates,
-              zoom: config.zoom,
-              essential: true,
-            });
-            document.getElementById("searchInput").value = event.target.textContent;
-          }
-          catch(error){
-            console.error("Result click error:", error);
-          }
+  async function onSearchResultClick(event) {
+    try {
+      const locationData = await getLocationData(event.target.id);
+      if(locationDta)
+        map.flyTo({
+          center: locationData.features[0].geometry.coordinates,
+          zoom: config.zoom,
+          essential: true,
+        });
+      document.getElementById("searchInput").value = event.target.textContent;
+    } catch (error) {
+      console.error("Result click error:", error);
+    }
   }
 
   function debounce(func, delay) {
     let timeoutId;
-    
-    return function() {
+
+    return function () {
       const context = this;
       const args = arguments;
-      
+
       clearTimeout(timeoutId);
       timeoutId = setTimeout(() => {
         func.apply(context, args);
       }, delay);
     };
   }
-  
 
   async function searchMapbox() {
-
     const searchInput = document.getElementById("searchInput").value.trim();
     const searchResultsDiv = document.getElementById("searchResults");
     if (searchInput === "" || searchInput === null || searchInput.length < 3) {
@@ -158,7 +159,7 @@ function init() {
       const name = feature.name;
       const resultDiv = document.createElement("div");
       resultDiv.innerHTML = `<p class="search-result-item" id="${feature.mapbox_id}">${name}</p>`;
-      resultDiv.addEventListener("click", onSearchResultClick)
+      resultDiv.addEventListener("click", onSearchResultClick);
       searchResultsDiv.appendChild(resultDiv);
     });
   }
@@ -170,7 +171,7 @@ function init() {
       // Number 13 is the "Enter" key on the keyboard
       // debounce(searchMapbox(), 300);
       const decouncedSearch = debounce(searchMapbox, 300);
-      decouncedSearch()
+      decouncedSearch();
       event.preventDefault();
     });
 }
