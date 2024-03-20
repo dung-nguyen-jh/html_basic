308f43b32a66f72fb450a2e974827b908cf5d3ea
dung-nguyen-jh <dung.nguyen.2@journeyh.io>
2024-03-20 18:00:39 +0700

    update responsive

--- Pixture/scripts.js
+++ Pixture/scripts.js
@@ -1,21 +1,23 @@
-const slidesTrack = document.querySelector('[data-carousel-slides]');
-const slides = Array.from(slidesTrack.children);
-const buttons = document.querySelectorAll('[data-carousel-button]');
-const slideWidth = slides[0].offsetWidth+30;
-slides.forEach((slide,index) => {
-    slide.style.left = slideWidth * index + 'px';
-});
+const carousels = document.querySelectorAll(".carousel");
+carousels.forEach((carousel) => {
+  const slidesTrack = carousel.querySelector("[data-carousel-slides]");
+  const slides = Array.from(slidesTrack.children);
+  const buttons = carousel.querySelectorAll("[data-carousel-button]");
+  const slideWidth = slides[0].offsetWidth + 30;
+  slides.forEach((slide, index) => {
+    slide.style.left = slideWidth * index + "px";
+  });
 
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        const offset = button.dataset.carouselButton === 'next' ? 1 : -1;
-        const activeSlide = slidesTrack.querySelector('[data-active]');
-        let index = slides.indexOf(activeSlide) + offset;
-        if (index >= slides.length) index = 0;
-        if (index < 0) index = slides.length - 1;
-        slidesTrack.style.transform = `translateX(-${slideWidth * index}px)`;
-        slidesTrack.querySelector('[data-active]').removeAttribute('data-active');
-        slides[index].setAttribute('data-active', '');
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+      const activeSlide = slidesTrack.querySelector("[data-active]");
+      let index = slides.indexOf(activeSlide) + offset;
+      if (index >= slides.length) index = 0;
+      if (index < 0) index = slides.length - 1;
+      slidesTrack.style.transform = `translateX(-${slideWidth * index}px)`;
+      slidesTrack.querySelector("[data-active]").removeAttribute("data-active");
+      slides[index].setAttribute("data-active", "");
     });
-}
-);
+  });
+});
